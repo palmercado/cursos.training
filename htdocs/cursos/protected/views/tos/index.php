eb3d7959ca836f905eae17fea8c40054e59f7890
iqzone <jb@iq-zone.com>
2012-07-20 04:29:11 -0500

    Añadido términos y condiciones

---
+++ htdocs/cursos/protected/views/tos/index.php
@@ -0,0 +1,41 @@
+<?php
+$this->breadcrumbs=array(
+	'Tos',
+);?>
+<h2><?php echo Yii::t( 'global', 'tos' ) ?></h2>
+
+<pre>
+Codebit training, Términos y condiciones
+
+1.- Contenidos
+
+Los servicios contratados con Codebit deben ser utilizados exclusivamente 
+con fines licitos. Queda estrictamente prohibido el uso de cualquiera de
+los servicios contratados con Codebit o materiales publicados por codebit
+para la violación cualquier Ley local, Nacional o Internacional.
+
+Codebit se deslinda de materiales o información que no haya sido extrictamente publicada en el sitio de Codebit.org
+
+En caso de cualquier uso del sistema para fines ilicitos, Codebit podrá suspender los servicios contratados sin previo aviso y sin comprometerse a
+realizar devolución alguna por el tiempo no utilizado de su servicio.
+
+2. Denegación o discuntinuidad del servicio
+Codebit se reserva el derecho a denegar o interrupir los servicios contratados por el cliente con o sin notificación previa si el cliente incurre en cualquier conducta o actividad que Codebit.org considere abuso o violación de alguno de los términos, normas y condiciones aquí expuestas, con o sin previo aviso y no responsabilizándose de las consecuencias que pudieran resultar por este motivo.
+
+
+3. Programas y Scripts
+El contenido de programas publicados por Codebit son de uso educativo, cualquier uso con fines ilicitos de los mismos son responsabilidad de quien los utiliza y mas no de Codebit.
+
+
+4. Confidencialidad
+Codebit tendrá la confidencialidad de seguridad de la información facilitada por el Cliente para el cobreo de servicios, tal como lo describe la Ley de Protección de Datos.
+
+5. Cancelación de inscripción
+De acuerdo a la fecha de solicitud se aplicará el porcentaje señalado en la tabla para el cálculo de la cancelación de saldos del participante:
+
+-  4 días antes del inicio del  curso 100%
+-  2 días antes del inicio del  curso  50%
+- 12 horas antes del inicio del curso   0%
+
+Codebit, y todos sus programas derivados, son Marca registrada y queda prohibido su uso sin alguna autorización.
+</pre>
